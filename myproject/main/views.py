016f780ae1c1fa203ae56577a5addbc8601c3b66
jafoor <abujafor.cse11@gmai.com>
2020-04-02 23:46:35 +0600

    Update and dynamic homepage

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , get_object_or_404, redirect
 from .models import Main
 from news.models import News
 from cat.models import Cat
+from subcat.models import SubCat
 # Create your views here.
 
 def home(request):
@@ -9,9 +10,11 @@ def home(request):
     # site = Main.objects.filter(pk=2)
     site = Main.objects.get(pk = 1)
     news = News.objects.all().order_by('-pk')
+    lastnews = News.objects.all().order_by('-pk')[:3]
     cat = Cat.objects.all()
+    subcat = SubCat.objects.all()
 
-    return render(request, 'front/home.html', {'site' : site, 'news': news, 'cat':cat})
+    return render(request, 'front/home.html', {'site' : site, 'news': news, 'cat':cat, 'subcat': subcat, 'lastnews': lastnews})
 
 def about(request):
     site = Main.objects.get(pk = 1)
